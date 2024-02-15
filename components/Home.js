034d6d9324ffe4e0c8b9b17b1267c81784bdf880
Your name <Your email>
2024-02-15 11:49:51 +0100

    front with tailwind

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,35 @@
 import styles from "../styles/Home.module.css";
 
 function Home() {
-    return (
-        <div>
-            <main className={styles.main}>
-                <h1 className="">
-                    Welcome to <a href="https://nextjs.org">Next.js!</a>
-                </h1>
-            </main>
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="flex-grow flex">
+        <div className="w-1/2 bg-base-300 flex justify-center items-center"></div>
+        <div className="w-1/2 bg-[#0f172a] flex justify-center items-center">
+          <div className="text-white">
+            <div>
+              <h1 className="text-3xl">What's happening</h1>
+              <h3 className="text-lg">Join Hackatweet today.</h3>
+            </div>
+            <div className="flex gap-4 mt-4">
+              <a
+                href="#"
+                className="py-2 px-4 bg-indigo-600 text-white rounded-lg transition-colors hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700"
+              >
+                Sign Up
+              </a>
+              <a
+                href="#"
+                className="py-2 px-4 bg-indigo-600 text-white rounded-lg transition-colors hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700"
+              >
+                Sign In
+              </a>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Home;
